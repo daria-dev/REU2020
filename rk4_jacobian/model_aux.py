fixed jacobian loss

rk4_jacobian/model_aux.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import solve_ivp
 from jacobian import *
+from jacobian_util import *
 
 def make_directory(dir_name):
     '''
@@ -174,12 +175,14 @@ def train_nn(train_y,val_y,net,criterion,optimizer,args):
                 num_point = y.shape[1]
                 num_J_points = math.floor(0.1*num_traj*num_point) # num points to calculate Jacobian on <= 0.1*num_traj*num_point
                 J = generate_jacobian(y,'Lorenz',num_J_points)
+                y = np.reshape(y, (y.shape[0]*y.shape[1], y.shape[2]))
             
                 jacobian_loss = torch.tensor(0.0)
                 weight = torch.tensor(args.jacobian_lambda) #lambda
-                '''for point in J: # boundary wall
-                    jacobian_loss += (point - torch.autograd.functional.jacobian(net.f, point)).norm()**2
-                loss += weight*jacobian_loss'''
+                for m in range(num_J_points): # boundary wall
+                    jacobian_loss += (torch.tensor(J[m,:,:], dtype=torch.float32) 
+                            - jacobian(net.f, torch.tensor(y[m,:], dtype=torch.float32))).norm()**2
+                loss += weight*jacobian_loss
                 
                 # add regularization if necessary
                 if args.Reg == 'L2':